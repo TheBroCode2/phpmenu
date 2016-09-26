ca7c55754d9612abdbf1b5b8c2dc5ac7279973b3
nikolaj dommergård <nikolajdommergaard@gmail.com>
2016-09-26 12:32:11 +0200

    Modul2PHP
    Modul 2 PHP Projekt
    -PHP Menu med current page funktion

---
+++ menu.php
@@ -0,0 +1,10 @@
+
+<nav>
+	<ul>
+		<li><a href="forside.php" <?php if ($curpage == 'forside.php'){echo 'class="active"';}?>>Forside</a></li>
+		<li><a href="underside1.php" <?php if ($curpage == 'underside1.php'){echo 'class="active"';}?>>Underside1</a></li>
+		<li><a href="underside2.php" <?php if ($curpage == 'underside2.php'){echo 'class="active"';}?>>Underside2</a></li>
+	</ul>
+</nav>
+
+
---
+++ underside2.php
@@ -0,0 +1,13 @@
+<?php $curpage = basename($_SERVER['PHP_SELF']); ?>
+<!doctype html>
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="styles.css">
+<meta charset="UTF-8">
+<title>Untitled Document</title>
+</head>
+
+<body>
+<?php include 'menu.php'; ?>
+</body>
+</html>
